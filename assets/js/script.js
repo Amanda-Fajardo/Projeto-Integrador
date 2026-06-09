e2a4b4c95c135b2ff7aeb8bf5d34ec7dbe0a6229
Amanda-Fajardo <amandafajardo.dev@gmail.com>
2026-06-09 12:14:04 -0300

    feat: criando arrays de objetos

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const acoes = [
+    {icone: "🚰", texto: 'Feche bem a torneira quando não estiver usando'},
+    {icone: "🔔", texto: 'Avise logo se perceber um vazamento'},
+    {icone: "♻️", texto: 'Reutilize água de limpeza quando possível'},
+    {icone: "🥤", texto: 'Use copo ou garrafa reutilizável'},
+    {icone: "💬", texto: 'Conte para seus colegas sobre essas dicas'},
+    {icone: "🌍", texto: 'Multiplique - cada pessoa influencia outras 5'}
+]
+
